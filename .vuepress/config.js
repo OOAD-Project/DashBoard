6d26a135412991170c547a6d867d5dbada658e00
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-05-12 21:53:07 +0800

    自动化构建GithubPages

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+/**
+ * Github Pages 配置
+ * https://vuepress.vuejs.org/zh/default-theme-config/
+ */
+module.exports = {
+  title: 'EasyOrder',
+  description: 'DashBoard',
+  base: '/DashBoard/',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' }
+    ]
+  }
+};
