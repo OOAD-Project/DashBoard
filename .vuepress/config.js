e65998d159a496eae36eeede039380834d498433
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-07-01 17:04:38 +0800

    final

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     base: '/DashBoard/',
     themeConfig: {
         nav: [
-            { text: '项目规划', link: '/1-About.md' },
+            { text: '期末总结', link: '/X3_Final_report.md' },
             { text: '团队组员', link: '/2-Team_profile.md' },
             { text: 'API', link: '/7-Design/7.3-API设计/' },
             { text: 'Github', link: 'https://github.com/OOAD-Project' }
